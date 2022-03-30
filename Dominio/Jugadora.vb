6e530bc4e53347fe374e79ff0126b25c29cefd7a
TheAnyer02 <39172295+TheAnyer02@users.noreply.github.com>
2022-03-30 11:07:17 +0200

    Add files via upload

---
+++ Dominio/Jugadora.vb
@@ -0,0 +1,40 @@
+﻿Public Class Jugadora
+
+    Public Property IDJugadora As Int16
+    Public Property Nombre As String
+    Public Property Fecha As Date
+    Public Property Puntos As Int16
+    Public Property Pais As String
+    Public ReadOnly Property PerDAO As JugadoraDAO
+
+    Public Sub New()
+        Me.PerDAO = New JugadoraDAO
+    End Sub
+
+    Public Sub New(id As String)
+        Me.PerDAO = New JugadoraDAO
+        Me.IDJugadora = id
+        Me.Puntos = 0
+    End Sub
+
+    Public Sub LeerTodasJugadoras()
+        Me.PerDAO.LeerTodas()
+    End Sub
+
+    Public Sub LeerJugadora()
+        Me.PerDAO.Leer(Me)
+    End Sub
+
+    Public Function InsertarJugadora() As Integer
+        Return Me.PerDAO.Insertar(Me)
+    End Function
+
+    Public Function ActualizarJugadora() As Integer
+        Return Me.PerDAO.Actualizar(Me)
+    End Function
+
+    Public Function BorrarJugadora() As Integer
+        Return Me.PerDAO.Borrar(Me)
+    End Function
+
+End Class
---
+++ Persistencia/JugadoraDAO.vb
@@ -0,0 +1,40 @@
+﻿Public Class JugadoraDAO
+
+    Public ReadOnly Property Jugadoras As Collection
+
+    Public Sub New()
+        Me.Jugadoras = New Collection
+    End Sub
+
+    Public Sub LeerTodas()
+        Dim p As Jugadora
+        Dim col, aux As Collection
+        col = AgenteBD.ObtenerAgente().Leer("SELECT * FROM Jugadoras ORDER BY IdJugadora")
+        For Each aux In col
+            p = New Jugadora(aux(1).ToString)
+            p.Nombre = aux(2).ToString
+            Me.Jugadoras.Add(p)
+        Next
+    End Sub
+
+    Public Sub Leer(ByRef p As Jugadora)
+        Dim col As Collection : Dim aux As Collection
+        col = AgenteBD.ObtenerAgente.Leer("SELECT * FROM Jugadoras WHERE IdJugadora='" & p.IDJugadora & "';")
+        For Each aux In col
+            p.Nombre = aux(2).ToString
+        Next
+    End Sub
+
+    Public Function Insertar(ByVal p As Jugadora) As Integer
+        Return AgenteBD.ObtenerAgente.Modificar("INSERT INTO Jugadoras VALUES ('" & p.IDJugadora & "', '" & p.Nombre & "');")
+    End Function
+
+    Public Function Actualizar(ByVal p As Jugadora) As Integer
+        Return AgenteBD.ObtenerAgente.Modificar("UPDATE Jugadoras SET NombrePersona='" & p.Nombre & "' WHERE IDPersona='" & p.IDJugadora & "';")
+    End Function
+
+    Public Function Borrar(ByVal p As Jugadora) As Integer
+        Return AgenteBD.ObtenerAgente.Modificar("DELETE FROM Jugadoras WHERE IDPersona='" & p.IDJugadora & "';")
+    End Function
+
+End Class
