b1d1ff62260ec26b6de097d1eedcb046058f835a
TheAnyer02 <39172295+TheAnyer02@users.noreply.github.com>
2022-04-10 16:36:32 +0200

    Add files via upload

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Torneo_Tenis.Form1
+            Me.MainForm = Global.Torneo_Tenis.Main
         End Sub
     End Class
 End Namespace
