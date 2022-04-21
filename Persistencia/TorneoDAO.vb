d391ee47676a0265073a5799de80528f42492d65
EDMolina <91478468+EDMolina@users.noreply.github.com>
2022-04-21 16:38:00 +0200

    Update TorneoDAO.vb

--- Persistencia/TorneoDAO.vb
+++ Persistencia/TorneoDAO.vb
@@ -5,36 +5,43 @@
         Me.Torneo = New Collection
     End Sub
 
+  Public Class TorneoDAO
+    Public ReadOnly Property Torneo As Collection
+
+    Public Sub New()
+        Me.Torneo = New Collection
+    End Sub
+
     Public Sub LeerTodas()
-        Dim p As Torneo
+        Dim t As Torneo
         Dim col, aux As Collection
         col = AgenteBD.ObtenerAgente().Leer("SELECT * FROM Torneos ORDER BY idTorneo")
         For Each aux In col
-            p = New Torneo(aux(1).ToString)
-            p.NombreTorneo = aux(2).ToString
-            p.CiudadTorneo = aux(3).ToString
-            p.PaisTorneo.idPais = aux(4).ToString
-            Me.Torneo.Add(p)
+            t = New Torneo(aux(1).ToString)
+            t.NombreTorneo = aux(2).ToString
+            t.CiudadTorneo = aux(3).ToString
+            t.PaisTorneo = New Pais(aux(4).ToString)
+            Me.Torneo.Add(t)
         Next
     End Sub
 
-    Public Sub Leer(ByRef p As Torneo)
+    Public Sub Leer(ByRef t As Torneo)
         Dim col As Collection : Dim aux As Collection
-        col = AgenteBD.ObtenerAgente.Leer("SELECT * FROM Torneos WHERE idTorneo='" & p.idTorneo & "';")
+        col = AgenteBD.ObtenerAgente.Leer("SELECT * FROM Torneos WHERE idTorneo='" & t.idTorneo & "';")
         For Each aux In col
-            p.NombreTorneo = aux(2).ToString
+            t.NombreTorneo = aux(2).ToString
         Next
     End Sub
 
-    Public Function Insertar(ByVal p As Torneo) As Integer
-        Return AgenteBD.ObtenerAgente.Modificar("INSERT INTO Torneos VALUES ('" & p.idTorneo & "', '" & p.NombreTorneo & "','" & p.CiudadTorneo & "', '" & p.PaisTorneo.idPais & "';")
+    Public Function Insertar(ByVal t As Torneo) As Integer
+        Return AgenteBD.ObtenerAgente.Modificar("INSERT INTO Torneos VALUES ('" & t.idTorneo & "', '" & t.NombreTorneo & "', '" & t.CiudadTorneo & "', '" & t.PaisTorneo.idPais & "');")
     End Function
 
-    Public Function Actualizar(ByVal p As Torneo) As Integer
-        Return AgenteBD.ObtenerAgente.Modificar("UPDATE Torneos SET NombreTorneo='" & p.NombreTorneo & "',  CiudadTorneo='" & p.NombreTorneo & "', PaisTorneo='" & p.PaisTorneo.idPais & "' WHERE idTorneo='" & p.idTorneo & "';")
+    Public Function Actualizar(ByVal t As Torneo) As Integer
+        Return AgenteBD.ObtenerAgente.Modificar("UPDATE Torneos SET NombreTorneo='" & t.NombreTorneo & "',  CiudadTorneo='" & t.NombreTorneo & "', PaisTorneo='" & t.PaisTorneo.idPais & "' WHERE idTorneo='" & t.idTorneo & "';")
     End Function
 
-    Public Function Borrar(ByVal p As Torneo) As Integer
-        Return AgenteBD.ObtenerAgente.Modificar("DELETE FROM Torneos WHERE idTorneo='" & p.idTorneo & "';")
+    Public Function Borrar(ByVal t As Torneo) As Integer
+        Return AgenteBD.ObtenerAgente.Modificar("DELETE FROM Torneos WHERE idTorneo='" & t.idTorneo & "';")
     End Function
 End Class
